43e19b0a4cca9c3751d0ecabfff4427b4b861315
Ricky Tran <sunbae134@gmail.com>
2017-06-21 15:40:54 +0700

    add scripts and update config

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,28 @@
-#!/bin/bash
+#! /bin/bash
 
-jekyll build --config _config_ci.yml
-htmlproofer ./_site --url-ignore www.youtube.com
+set -eu
+
+DEPLOY_REPO="https://${DEPLOY_BLOG_TOKEN}@github.com/hoangtn1304/hoangtn1304.github.io.git"
+
+function main {
+	clean
+	get_current_site
+	build_site
+}
+
+function clean { 
+	echo "cleaning _site folder"
+	if [ -d "_site" ]; then rm -Rf _site; fi 
+}
+
+function get_current_site { 
+	echo "getting latest site"
+	git clone --depth 1 $DEPLOY_REPO _site 
+}
+
+function build_site { 
+	echo "building site"
+	bundle exec jekyll build 
+}
+
+main
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
+#! /bin/bash
 
-if[[ $TRAVIS_BRANCH == 'gh-pages' ]]; then
-    cd _site
-    git init
-    
-    git config user.name "Travis CI"
-    git config user.email "hoangtn1304@gmail.com"
-    
-    git add .
-    git commit -m "Travis CI deploy"
-    
-    # We redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-    git push --force --quite "https://${git_user}:${git_password}@${git_target}" master:master > dev/null 2>&1
-else
-    echo "Invalid branch. You can only deploy from gh-pages."
-    exit 1
+set -eu
+
+DEPLOY_REPO="https://${DEPLOY_BLOG_TOKEN}@github.com/hoangtn1304/hoangtn1304.github.io.git"
+
+echo "deploying changes"
+
+if [ -z "$TRAVIS_PULL_REQUEST" ]; then
+    echo "except don't publish site for pull requests"
+    exit 0
+fi  
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    echo "except we should only publish the master branch. stopping here"
+    exit 0
 fi
+
+cd _site
+git config --global user.name "Travis CI"
+git config --global user.email hoangtn1304@gmail.com
+git add -A
+git status
+git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+exit `git push $DEPLOY_REPO master:master`
